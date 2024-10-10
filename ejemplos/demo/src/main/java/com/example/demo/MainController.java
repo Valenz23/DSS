05c0085717d39929a35ce00bc7530cd38220769d
Pablo Valenzuela Álvarez <valenbetis@gmail.com>
2024-10-10 10:14:26 +0200

    updt demo

--- ejemplos/demo/src/main/java/com/example/demo/MainController.java
+++ ejemplos/demo/src/main/java/com/example/demo/MainController.java
@@ -1,11 +1,9 @@
-package com.example.demo.controller;
+package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.example.demo.repository.StudentRepository;
 
 @Controller
 public class MainController {
