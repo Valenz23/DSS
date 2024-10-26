d380843f53be9c33e2fde5dfd6bd059f15347cd7
Pablo Valenzuela Álvarez <valenbetis@gmail.com>
2024-10-26 12:28:46 +0200

    pdf generado
    opcion que genera un pdf cuando compras + cosas de diseño al comprar

--- practica1/src/main/java/dss/pvalenz23/practica1/controladores/ControladorCarrito.java
+++ practica1/src/main/java/dss/pvalenz23/practica1/controladores/ControladorCarrito.java
@@ -1,6 +1,12 @@
 package dss.pvalenz23.practica1.controladores;
 
+import java.io.FileNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.itextpdf.text.DocumentException;
+
 import dss.pvalenz23.practica1.modelos.Producto;
 import dss.pvalenz23.practica1.servicios.ServicioCarrito;
+import dss.pvalenz23.practica1.servicios.ServicioPDF;
 
 @Controller
 @RequestMapping("carrito")
@@ -18,8 +27,11 @@ public class ControladorCarrito {
     @Autowired
     private ServicioCarrito servicioCarrito;
 
+    @Autowired
+    private ServicioPDF servicioPDF;
+
     @GetMapping
-    private String getCarrito(Model model){        
+    public String getCarrito(Model model){        
         model.addAttribute("productosCarrito", servicioCarrito.getCarrito());
 
         
@@ -31,8 +43,8 @@ public class ControladorCarrito {
         return "carrito";
     }
 
-    @PostMapping("/add")
-    private String addProductoCarrito(@RequestParam("id") Long id, @RequestParam("nombre") String nombre, @RequestParam("precio") double precio){
+    @PostMapping("add")
+    public String addProductoCarrito(@RequestParam("id") Long id, @RequestParam("nombre") String nombre, @RequestParam("precio") double precio){
         Producto nuevo = new Producto();
         nuevo.setId(id);
         nuevo.setNombre(nombre);
@@ -41,16 +53,35 @@ public class ControladorCarrito {
         return "redirect:/carrito";
     }
 
-    @PostMapping("/delete")
-    private String deleteProductoCarrito(@RequestParam("id") Long id){        
+    @PostMapping("delete")
+    public String deleteProductoCarrito(@RequestParam("id") Long id){        
         servicioCarrito.deleteProductoCarritoById(id);
         return "redirect:/carrito";
     }
 
-    @GetMapping("/clear")
-    private String clearCarrito(){
+    @GetMapping("clear")
+    public String clearCarrito(){
         servicioCarrito.clearCarrito();
         return "redirect:/carrito";
+    }
+
+    @GetMapping("comprar")
+    public ResponseEntity<byte[]> generarPdfCompra(Model model) throws FileNotFoundException {
+
+        try {
+            byte[] pdfBytes = servicioPDF.generarPdfCompra(servicioCarrito.getCarrito());
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDispositionFormData("inline", "compra.pdf");
+            servicioCarrito.clearCarrito();
+
+            return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
+
+        } catch (DocumentException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
